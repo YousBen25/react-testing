4d8fb215dce3627ddbbfc1a62cbf62df2bfacc23
Youssef Benabdallah <youssefbenabdallah@hotmail.fr>
2021-01-13 16:25:40 +0100

    completed

--- src/helpers/string.js
+++ src/helpers/string.js
@@ -1,5 +1,5 @@
 export default function capitalize(word) {
-  if (!word) { // this guard clause handles null and empty string
+ if (!word) { // this guard clause handles null and empty string
     return word;
   }
   return `${word[0].toUpperCase()}${word.substr(1).toLowerCase()}`;
